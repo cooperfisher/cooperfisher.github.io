2b57b09a8cdccd3d27fa9cf887db4bca341fab67
Cooper <cooperfisher@byui.edu>
2018-04-11 22:10:49 -0600

    Final Term Website

---
+++ mountain-spoke/js/services.js
@@ -0,0 +1,58 @@
+var h = document.querySelector('header');
+var s = document.querySelector('section');
+
+var requestURL = "data/services.json";
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    var repairShop = request.response;
+    populateHeader(repairShop);
+    showHeroes(repairShop);
+}
+
+function populateHeader(jsonObj) {
+  var myH1 = document.createElement('h1');
+  myH1.textContent = jsonObj['repairShop'];
+    
+  h.appendChild(myH1);
+
+  var myPara = document.createElement('p');
+  myPara.textContent = 'Wheels ' + jsonObj['$10'] + jsonObj['$20'];
+  h.appendChild(myPara);
+}
+
+function showRepairs(jsonObj) {
+  var repairs = jsonObj['repairs'];
+      
+  for (var i = 0; i < heroes.length; i++) {
+    var myArticle = document.createElement('article');
+    var myH2 = document.createElement('h2');
+    var myPara1 = document.createElement('p');
+    var myPara2 = document.createElement('p');
+    var myPara3 = document.createElement('p');
+    var myList = document.createElement('ul');
+
+    myH2.textContent = heroes[i].name;
+    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+    myPara2.textContent = 'Age: ' + heroes[i].age;
+    myPara3.textContent = 'Superpowers:';
+        
+    var superPowers = heroes[i].powers;
+    for (var j = 0; j < superPowers.length; j++) {
+      var listItem = document.createElement('li');
+      listItem.textContent = superPowers[j];
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    s.appendChild(myArticle);
+  }
+}
